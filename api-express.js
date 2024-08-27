d3e9d701605c51e1a149876592edf4d078c7a847
Sales <contabilgs3@gmail.com>
2024-08-27 17:17:41 -0300

    create api to connection with DB & request test

---
+++ api-express.js
@@ -0,0 +1,34 @@
+// Cria servidor Local utilizando biblioteca Express do Node
+// Posteriormente atualizar com biblioteca HTTP para deploy
+
+const express = require('express');
+const cors = require('cors')
+const app = express();
+
+app.listen(8000, ()=> {
+    console.log('Servidor Express Live! http://localhost:8000/')
+});
+
+app.use(cors({
+    origin: "http://127.0.0.1:5500"
+}))
+
+// Cria conexão com Banco de Dados do RailWay
+
+const mysql = require('mysql2/promise');
+
+const DBcon = mysql.createPool('mysql://root:otJKDeDzWueXZjjjxsKNYaEcnyUCcFvE@junction.proxy.rlwy.net:58360/railway')
+
+// Routes :::
+
+app.get('/', async function(req, res){
+
+    let [query] = await DBcon.execute('SELECT * FROM tbl_TaskManager');
+
+    res.send(query);
+})
+
+app.post('/', async function(req, res){
+    
+    let [query] = await DBcon.execute('INSERT INTO tbl_TaskManager(task_check, task_description, task_vecto)')
+})
--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 window.onload = function(){
+
+    // Esconde a tela de criar nova tarefa
     document.querySelector('.newTask').classList.add('hidden');
+
+    //
+    collectData()
+
+}
+
+async function collectData(){
+
+    let req = await fetch('http://localhost:8000/').catch(data => {console.log(data)})
+
 }
 
 
